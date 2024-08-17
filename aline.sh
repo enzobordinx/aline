17f755fbceb515921b731ae4dce563ad72ccc536
Enzo Xavier <85846502+enzobordinx@users.noreply.github.com>
2024-08-16 22:05:44 -0300

    aline.sh

--- aline.sh
+++ aline.sh
@@ -1,15 +1,14 @@
-#!/bin/bash# Limpar o terminal
+#!/bin/bash
 clear
+sleep 2
 
-# Pausa de 2 segundossleep 2
-
-# Mensagem a ser exibida
-message="ALINE EU TE AMO MEU XUXU, HOJE VOCÊ VAI SER MIMADA"# Função para encontrar e exibir a letra corretafunctionfind_letter() {
+message="ALINE EU TE AMO MEU XUXU, HOJE VOCÊ VAI SER MIMADA"functionfind_letter() {
     local target=$1local alphabet=({A..Z})
 
-    for letter in"${alphabet[@]}"; doecho -ne "$letter\r"# Exibe a letra e volta ao início da linhasleep 0.05            # Pausa curta para criar o efeito de animaçãoif [[ "$letter" == "$target" ]]; thenecho -n "$letter"# Mantém a letra quando ela é encontradabreakfidone
+    for letter in"${alphabet[@]}"; doecho -ne "$letter\r"sleep 0.05
+        if [[ "$letter" == "$target" ]]; thenecho -n "$letter"breakfidone
 }
 
-# Escrever a mensagem letra por letrafor ((i=0; i<${#message}; i++)); do
+for ((i=0; i<${#message}; i++)); do
     char="${message:$i:1}"if [[ "$char" =~ [A-Z] ]]; then
-        find_letter "$char"elseecho -n "$char"# Escreve diretamente caracteres que não são letras maiúsculasfidone# Nova linha ao final da execuçãoecho
+        find_letter "$char"elseecho -n "$char"fidoneecho
